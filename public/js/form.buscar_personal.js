f189125cc2264cea21383163c26aed6cc29fe894
Federico <federico.demeco@hotmail.com>
2024-04-18 22:24:41 -0300

    Se agregó la funcionalidad para buscar personal y para que genere una tabla de busqueda

--- public/js/form.buscar_personal.js
+++ public/js/form.buscar_personal.js
@@ -1,3 +1,35 @@
+//Funcion para devolver la fecha actual
+
+function fecha_hoy(){
+
+    const fecha_hoy = new Date()
+
+    const fechaISO = fecha_hoy.toISOString();
+
+    const fechaFormateada = fechaISO.slice(0, 19).replace('T', ' ');
+
+    const fecha_separada = fechaFormateada.split(' ')
+
+    const fecha = fecha_separada[0]
+
+    return fecha
+
+}
+
+//Funcion para convertir una fecha
+function convertirFecha(fecha){
+
+    const fecha_separada = fecha.split('-')
+    const dia = fecha_separada[2]
+    const mes = fecha_separada[1]
+    const anio = fecha_separada[0]
+
+    const fecha_nueva = `${dia}/${mes}/${anio}`
+
+    return fecha_nueva
+
+}
+
 // Funcion para capitalizar el texto
 function capitalizar(texto){
 
@@ -19,7 +51,7 @@ function tabla_busqueda(json){
     // Creando los encabezados de la tabla
 
     const thead = document.createElement('thead');
-    const encabezados = ['Nombre', 'Apellido', 'DNI', 'Edad', 'Fecha de ingreso', 'Dias de vacaciones', 'Acciones'];
+    const encabezados = ['Nombre', 'Apellido', 'DNI', 'Edad', 'Fecha de ingreso', 'Dias de vacaciones', 'Estado', 'Acciones'];
 
     // Creando la fila del encabezado
     const encabezadosRow = document.createElement('tr');
@@ -49,29 +81,99 @@ function tabla_busqueda(json){
         const tdEdad = document.createElement('td');
         const tdFechaIngreso = document.createElement('td');
         const tdVacaciones = document.createElement('td');
-        const tdDeshabilitar = document.createElement('button')
+        const tdEstado = document.createElement('td');
+        const tdDeshabilitar = document.createElement('button');
+
+        // Se calcula la edad de la persona
+        const diferenciaFechasEdad = new Date() - new Date (dato.nacimiento);
+        const convertirSegAnios = 1000 * 60 * 60 * 24 * 365.25;
+        const edad = Math.floor(diferenciaFechasEdad/convertirSegAnios);
+
+        // Se calcula las vacaciones segun la fecha de ingreso
+
+        const diferenciaFechasIngreso = new Date() - new Date(dato.ingreso);
+        const convertirSegDias = 1000 * 60 * 60 * 24;
+        const diasActualIngreso = Math.floor(diferenciaFechasIngreso/convertirSegDias)
+
+        const tresMesesPromDias = 30*3
+        const seisMesesPromDias = 30*6
+        const cincoAnios = 365.25*5
+        const diezAnios = 365.25*10
+        const veinteAnios = 365.25*20
+
+        let vacaciones;
+
+        if(diasActualIngreso < tresMesesPromDias){
+
+            vacaciones = Math.floor(diasActualIngreso/20) + ' dias'
+
+        }else if(diasActualIngreso >= tresMesesPromDias && diasActualIngreso < seisMesesPromDias ){
+
+            vacaciones = 7 + ' dias'
+
+        }else if(diasActualIngreso >= seisMesesPromDias && diasActualIngreso < cincoAnios){
+
+            vacaciones = 14 + ' dias'
+
+        }else if(diasActualIngreso >= cincoAnios && diasActualIngreso < diezAnios){
+
+            vacaciones = 21 + ' dias'
+
+        }else if(diasActualIngreso >= diezAnios && diasActualIngreso < veinteAnios){
+
+            vacaciones = 28 + ' dias'
+
+        }else{
+
+            vacaciones = 35 + ' dias'
+
+        }
+
+        // Se cargan los datos en cada fila
 
         tdNombre.textContent = dato.nombre;
         tdApellido.textContent = dato.apellido;
         tdDNI.textContent = dato.dni;
-        tdEdad.textContent = dato.nacimiento;
-        tdFechaIngreso.textContent = dato.ingreso;
-        tdVacaciones.textContent = 'A calcular'
-        tdDeshabilitar.textContent = 'Deshabilitar'
+        tdEdad.textContent = `${edad} años`;
+        tdFechaIngreso.textContent = convertirFecha(dato.ingreso);
+        tdEstado.textContent = ''
+        tdVacaciones.textContent = vacaciones;
+        tdDeshabilitar.textContent = 'Deshabilitar';
+
 
         tdDeshabilitar.addEventListener('click', async (e)=> {
 
-            console.log('Deshabilitado')
+            // Creo la confirmacion para realizar la operacion 
+
+            let confirmacion = confirm(`Deseas ${tdDeshabilitar.textContent} a ${dato.nombre} ${dato.apellido}?`)
+
+            if(confirmacion){
+
+                if(tdDeshabilitar.textContent == 'Deshabilitar'){
+
+                    tdVacaciones.textContent = '-'
+                    tdEstado.textContent = 'Baja'
+                    tdDeshabilitar.textContent = 'Habilitar'
+
+                }else{
+
+                    tdVacaciones.textContent = vacaciones
+                    tdEstado.textContent = 'Ok'
+                    tdDeshabilitar.textContent = 'Deshabilitar'
+
+                }
+            }
 
         })
 
         fila.appendChild(tdNombre);
-        fila.appendChild(tdApellido)
-        fila.appendChild(tdDNI)
-        fila.appendChild(tdEdad)
-        fila.appendChild(tdFechaIngreso)
-        fila.appendChild(tdVacaciones)
-        fila.appendChild(tdDeshabilitar)
+        fila.appendChild(tdApellido);
+        fila.appendChild(tdDNI);
+        fila.appendChild(tdEdad);
+        fila.appendChild(tdFechaIngreso);
+        fila.appendChild(tdVacaciones);
+        fila.appendChild(tdEstado);
+        fila.appendChild(tdDeshabilitar);
 
         tbody.appendChild(fila);
 
@@ -82,10 +184,11 @@ function tabla_busqueda(json){
     // Añadir tabla al contenedor del html
 
     const contenedorTabla = document.getElementById('tabla_busqueda');
-    contenedorTabla.innerHTML = '' //Limpiar contenido de busqueda anterior
-    contenedorTabla.appendChild(tabla)
+    contenedorTabla.innerHTML = ''; //Limpiar contenido de busqueda anterior
+    contenedorTabla.appendChild(tabla);
 
 }
+
 // Funcion para buscar los datos guardados en el local storage
 function leerDatos(){
 
@@ -101,15 +204,15 @@ function leerDatos(){
 
     if(data.busqueda == ''){
 
-        let storedData = JSON.parse(localStorage.getItem('Personal'))
+        let storedData = JSON.parse(localStorage.getItem('Personal'));
 
-        tabla_busqueda(storedData)
+        tabla_busqueda(storedData);
 
     }else{
 
-        console.log(data.busqueda)
+        console.log(data.busqueda);
 
-        document.getElementById('buscar').value = ''
+        document.getElementById('buscar').value = '';
 
     }
 
