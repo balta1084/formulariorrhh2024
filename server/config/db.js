242c403922af3ad9155b07d463bd3386224e20df
Federico <federico.demeco@hotmail.com>
2024-07-04 20:23:09 -0300

    Se agregó el manejo de errores de la conexion a la db

--- server/config/db.js
+++ server/config/db.js
@@ -5,17 +5,26 @@ dotenv.config();
 
 function conectar(){
 
-    const config = {
-        host: process.env.HOST_DB,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.NOMBRE_DB,
-        connectionLimit: 5
-    };
+    try{
 
-    const conectar = mysql.createConnection(config)
+        const config = {
+            host: process.env.HOST_DB,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.NOMBRE_DB,
+            connectionLimit: 5
+        };
+    
+        const conectar = mysql.createConnection(config)
+    
+        return conectar
 
-    return conectar
+    }catch(error){
+
+        console.error('Error al conectar a la base de datos:', error);
+        throw error;
+
+    }
 
 }
 
